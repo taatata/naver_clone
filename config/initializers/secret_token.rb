f8ca660b81c6cf30e86620cf660be146f65a7743
taatata <taatata@aol.jp>
2015-08-01 17:29:36 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NaverClone::Application.config.secret_key_base = '70e67146f0d637ef3e0bef858ac01a13fac71b9447eb93a079d7cb038e6fbb62bf1fd77ca79e69f02da9e2428e486c8bf4725400aef4757d620ef7684763f634'
