45576fad5601bcc4633f1c0fdbac00513697d184
taatata <taatata@aol.jp>
2015-08-14 18:02:48 +0900

    signed_in_userがmatomeでも必要だったのでSessionのHelperへ移動

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
 
   def index
@@ -45,13 +45,6 @@ class UsersController < ApplicationController
     end
 
     # Before actions
-
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
 
     def correct_user
       @user = User.find(params[:id])
