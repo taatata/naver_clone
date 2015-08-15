07b80fc235eb590d3c0e06af183e14514f1a075b
taatata <taatata@aol.jp>
2015-08-15 15:12:51 +0900

    matome.titleの長さを50に制限

--- app/models/matome.rb
+++ app/models/matome.rb
@@ -3,4 +3,5 @@ class Matome < ActiveRecord::Base
   default_scope -> { order('created_at DESC') }
   validates :content, presence: true
   validates :user_id, presence: true
+  validates :title,   presence: true, length: { maximum: 50 }
 end
