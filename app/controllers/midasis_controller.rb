267c795933a0bdc1c15d6e3b5f38be00ca1321f9
taatata <taatata@aol.jp>
2015-08-30 00:29:53 +0900

    不要な部分削除

--- app/controllers/midasis_controller.rb
+++ app/controllers/midasis_controller.rb
@@ -1,19 +1,2 @@
 class MidasisController < ApplicationController
-  def create
-    @midasi_new = Midasi.new(midasi_params)
-    @midasi_new.block_id = @block.id
-    @midasi_new.save
-    @matome = Matome.find(@midasi_new.matome_id)
-    @midasi = @matome.midasis.build # new for form
-    respond_to do |format|
-      format.html { redirect_to edit_matome_url( id: @matome.id ) }
-      format.js
-    end
-  end
-
-  private
-
-    def midasi_params
-      params.require(:midasi).permit(:content)
-    end
 end
