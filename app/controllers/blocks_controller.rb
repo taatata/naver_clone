94ec2960c9730fc704c4206630048b8fa2ccece1
taatata <taatata@aol.jp>
2015-09-01 01:26:02 +0900

    削除実装

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -1,2 +1,19 @@
 class BlocksController < ApplicationController
+  def destroy
+    block = Block.find(params[:id])
+
+    block_order = block.order
+    matome_id = block.matome_id
+    matome = Matome.find(matome_id)
+
+    block.destroy
+
+    matome.blocks.each do |b|
+      if b.order > block_order
+        b.order -= 1
+      end
+    end
+
+    redirect_to edit_matome_url(id: matome_id)
+  end
 end
