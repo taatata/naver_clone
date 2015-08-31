b645d1991d30df9b810cf652f68410437f802c4a
taatata <taatata@aol.jp>
2015-08-31 20:18:20 +0900

    ogpの取得をHelperでやる

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,16 +1,9 @@
 class LinksController < ApplicationController
   def create
     # get ogp by nokogiri
-    # yahoo は User-Agetの偽装必須
-    user_agent = 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)'
-    url = "http://www.yahoo.co.jp"
-    html = open(url, 'User-Agent' => user_agent).read
+    url = params[:ogp_url]
+    @ogp = get_ogp url
 
-    require 'nokogiri'
-    doc = Nokogiri::HTML.parse(html, nil)
-    title = doc.xpath("//meta[@property='og:title']/@content").text
-
-    @ogp = {title: title}
     respond_to do |format|
       format.html
       format.js
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def get_ogp(url)
+    # yahoo は User-Agetの偽装必須
+    user_agent = 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)'
+    html = open(url, 'User-Agent' => user_agent).read
+
+    require 'nokogiri'
+    doc = Nokogiri::HTML.parse(html, nil)
+    title       = doc.xpath("//meta[@property='og:title']/@content").text
+    description = doc.xpath("//meta[@property='og:description']/@content").text
+    q_url       = doc.xpath("//meta[@property='og:url']/@content").text
+    image_src   = doc.xpath("//meta[@property='og:image']/@content").text
+
+    ogp = {title: title, description: description, url: q_url, image_src: image_src}
+  end
 end
